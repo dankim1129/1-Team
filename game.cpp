c55461871f6d426df14745d8b458c9ef7bb5148e
jaewonwon <jaewonwon11@gmail.com>
2020-12-11 15:04:12 +0900

    adjust game code after pull

--- game.cpp
+++ game.cpp
@@ -36,6 +36,24 @@ void DrawDino(int dinoY)
 	}
 }
 
+void DrawCoin(int coinX)
+{
+	GotoXY(coinX, COIN_BOTTOM_Y);
+	printf("++");
+	GotoXY(coinX, COIN_BOTTOM_Y + 1);
+	printf("++");
+}
+
+//점수 감소 코인을 그리는 함수
+
+void DrawCoin2(int coin2X)
+{
+	GotoXY(coin2X, COIN_BOTTOM_Y);
+	printf("--");
+	GotoXY(coin2X, COIN_BOTTOM_Y + 1);
+	printf("--");
+}
+
 //나무를 그리는 함수
 void DrawTree(int treeX)
 {
@@ -85,38 +103,116 @@ bool isCollision(const int treeX, const int dinoY)
 	return false;
 }
 
+bool isCoin(const int coinX, const int dinoY)
+{
+	if (coinX == 6 && dinoY < 8)
+		return true;
+	return false;
+}
+
+bool isCoin2(const int coin2X, const int dinoY)
+{
+	if (coin2X == 6 && dinoY < 8)
+		return true;
+	return false;
+}
+
+
 void GameStart()
 {
+	//게임 시작시 초기화
 	bool isJumping = false;
+	bool goingup = false;
+	bool goingdown = false;
+	bool isdoubleJumping = false;
 	bool isBottom = true;
 	const int gravity = 3;
 
 	int dinoY = DINO_BOTTOM_Y;
 	int treeX = TREE_BOTTOM_X;
+	int coinX = TREE_BOTTOM_X + 1;
+	int coin2X[COIN_CNT];
 
-	int score = 0;
+	int score = 0, score2 = 0;	//score : time, score2 : 코인으로 얻은 점수
+	int life = 10;		//시작 목숨 갯수 초기화
 	clock_t start, curr;	//점수 변수 초기화
 	start = clock();		//시작시간 초기화
+	int tmp1 = 0;
+	bool check2 = false;
+	bool coinCheck[COIN_CNT];				//코인 등장 시간대 조절 
+	for (int i = 0; i < COIN_CNT; i++)
+		coinCheck[i] = false;
+
+	for (int i = 0; i < COIN_CNT; i++)
+		coin2X[i] = TREE_BOTTOM_X + 1;
+
+	int cnt = 0;							//코인 등장 시간 측정용
 
 	while (true)	//한 판에 대한 루프
 	{
+		//코인 등장 시간 지연 시킴
+
+		cnt++;
+		for (int i = 0; i < COIN_CNT; i++)
+			if (cnt == i * 6 + 6)
+				coinCheck[i] = true;
+
+		if (tmp1 < 5)
+			tmp1++;
+
 		//(v2.0) 충돌체크 트리의 x값과 공룡의 y값으로 판단
-		if (isCollision(treeX, dinoY))
+		if (isCollision(treeX, dinoY)) {
+			check2 = true;
+			tmp1 = 0;
+		}
+
+		if (tmp1 == 2 && check2) {
+			life--;
+			check2 = false;
+		}
+
+		//점수 획득 코인을 먹을 시
+		if (isCoin(coinX, dinoY))
+			score2++;
+
+		//점수 감소 코인을 먹을 시
+		for (int i = 0; i < COIN_CNT; i++)
+			if (coinCheck[i] && isCoin2(coin2X[i], dinoY))
+				score2--;
+
+		// 생명이 0이면 게임 종료
+
+		if (life == 0)
 			break;
 
-		//z키가 눌렸고, 바닥이 아닐때 점프
+
+
+		//z키가 눌렸고, 바닥일 때 점프
 		if (GetKeyDown() == 'z' && isBottom)
 		{
 			isJumping = true;
+			goingup = true;
+			goingdown = false;
 			isBottom = false;
 		}
 
+		//z키가 눌렸고, 점프 중일 때 점프
+		if (GetKeyDown() == 'z' && isJumping == true)
+		{
+			goingup = true;
+			goingdown = false;
+			isdoubleJumping = true;
+			isJumping = false;
+		}
+
+
 		//점프중이라면 Y를 감소, 점프가 끝났으면 Y를 증가.
-		if (isJumping)
+
+		if (goingup)
 		{
 			dinoY -= gravity;
 		}
-		else
+		else if (goingdown)
 		{
 			dinoY += gravity;
 		}
@@ -125,6 +221,7 @@ void GameStart()
 		if (dinoY >= DINO_BOTTOM_Y)
 		{
 			dinoY = DINO_BOTTOM_Y;
+			goingdown = false;
 			isBottom = true;
 		}
 
@@ -135,14 +232,39 @@ void GameStart()
 		{
 			treeX = TREE_BOTTOM_X;
 		}
+		//동전이 왼쪽으로 (x음수) 가도록하고
 
-		//점프의 맨위를 찍으면 점프가 끝난 상황.
-		if (dinoY <= 3)
+		//동전의 위치가 왼쪽 끝으로가면 다시 오른쪽 끝으로 소환.
+		coinX -= 2;
+		if (coinX <= 0)
+			coinX = TREE_BOTTOM_X + 1;
+
+		for (int i = 0; i < COIN_CNT; i++)
+			if (coinCheck[i]) {
+				coin2X[i] -= 2;
+				if (coin2X[i] <= 0)
+					coin2X[i] = TREE_BOTTOM_X + 1;
+			}
+
+		//점프의 한번으로 점프가 끝난 상황.
+		if (dinoY <= 3 && isdoubleJumping == true)
 		{
-			isJumping = false;
+			goingup = false;
+			goingdown = true;
+		}
+
+		//점프의 두번으로 점프가 끝난 상황.
+		if (dinoY <= 8 && isJumping == true)
+		{
+			goingup = false;
+			goingdown = true;
 		}
 
 		DrawDino(dinoY);		//draw dino
+		DrawCoin(coinX);		//draw Coin
+		for (int i = 0; i < COIN_CNT; i++)
+			if (coinCheck[i])
+				DrawCoin2(coin2X[i]);
 		DrawTree(treeX);		//draw Tree
 
 		//(v2.0)
@@ -157,11 +279,14 @@ void GameStart()
 
 		//(v2.0) 점수출력을 1초마다 해주는것이 아니라 항상 출력해주면서, 1초가 지났을때 ++ 해줍니다.
 		GotoXY(22, 0);	//커서를 가운데 위쪽으로 옮긴다. 콘솔창이 cols=100이니까 2*x이므로 22정도 넣어줌
-		printf("Score : %d ", score);	//점수 출력해줌.
+		printf("Score : %d\t", score + score2);	//점수 출력해줌.
+		printf("Life : %d\t", life);
+
 	}
 
 	//(v2.0) 게임 오버 메뉴
 	DrawGameOver(score);
+
 }
 
 //pull request test
--- game.h
+++ game.h
@@ -2,9 +2,13 @@
 #define DINO_BOTTOM_Y 12
 #define TREE_BOTTOM_Y 20
 #define TREE_BOTTOM_X 45
+#define COIN_BOTTOM_Y 7
+#define COIN_CNT 3
 
 void DrawDino(int dinoY);
 void DrawTree(int treeX);
 void DrawGameOver(const int score);
 bool isCollision(const int treeX, const int dinoY);
 void GameStart();
+bool isCoin(const int coinX, const int dinoY);
+bool isCoin2(const int coin2X, const int dinoY);
