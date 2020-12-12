d8b01f252dbff221d5edcf23e4688e7b8187e5f1
jaewonwon <jaewonwon11@gmail.com>
2020-12-12 22:55:05 +0900

    main update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include "interface.h"
 #include "util.h"
 #include "game.h"
+#include "score.h"
+
+// class h 선언 : 최근 게임점수 저장
+highscore h = highscore();
+
 int main() {
+
+	int menu_num;
+	
 	while (1) {
 		Init();
 		StartDraw();
-		int menu_num = MenuDraw();
-		if (menu_num == 0)  // 게임시작 버튼이 눌렸으면
+		Update(h.name, h.score);
+		menu_num = MenuDraw();
+
+		if (menu_num == 0) {// 게임시작 버튼이 눌렸으면 
 			GameStart();
-		if (menu_num == 1)
-			ShowScore();
-		if (menu_num == 2)
+		}
+		else if (menu_num == 1) // 점수 출력
+			ScorePrint();
+		else if(menu_num == 2) // 종 료
 			break;
 	}
 
