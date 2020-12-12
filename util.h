bb6907d447d5fe5982f6b481a3b09bfd8df30105
jaewonwon <jaewonwon11@gmail.com>
2020-12-12 22:53:24 +0900

    utility update

--- util.h
+++ util.h
@@ -1,9 +1,16 @@
 #pragma once
+
+
+// 처음 초기화를 하거나 커서위치를 바꾸거나 키보드 입력이 들어왔는지 확인하는 헤더
+
 #include <iostream>
+#include <string>
 #include <cstdio>
 #include <windows.h>
 #include <conio.h>
 
+using namespace std;
+
 void Init();
-void GotoXY(int x, int y);
+void GotoXY(int, int);
 int GetKeyDown();
